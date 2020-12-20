155486c49fafecb781f44ff0ddc6c54d7b9ae5bc
ddxman <sacrifice7355608@gmail.com>
2020-12-20 15:10:05 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,37 +1,22 @@
 var conteinerForLSB = document.getElementById('conteinerForLSB');
 var burger = document.getElementById('burger');
-
-conteinerForLSB.style.display='none';
- 	 			
-	burger.onclick=function(){
-		
-
-	if(conteinerForLSB.style.display =='none')
-	{
-		main.style.gridTemplateColumns='2fr 3fr';
-		conteinerForLSB.style.display='block';
-		conteinerForLSB.style.gridColumnStart='1';
-		MainContent.style.gridColumnStart='2';
-		Advirstment.style.display ='none';
-		
-
-	}
-	else if(conteinerForLSB.style.display=='block'){
-
-		main.style.gridTemplateColumns='3fr 1fr';
-		conteinerForLSB.style.display ='none';
-		
-		MainContent.style.gridColumnStart='1';
-		Advirstment.style.display ='block';
+var advirstment = document.querySelector('Advirstment');
+var mainContent = document.getElementById('MainContent');
 
 
-		Advirstment.style.gridColumnStart='2';
-	}
+var clientidth = document.documentElement.clientWidth;	
 
+if(clientidth<702)
+{
+	main.style.gridTemplateColumns ='3fr 1fr';
+	conteinerForLSB.style.display='none';
+	Advirstment.style.display ='block';
+	MainContent.style.gridColumnStart='1';
+	Advirstment.style.gridColumnStart='2';
 
-	
-};
-window.addEventListener("resize",function(){
+}
+
+	window.addEventListener("resize",function(){
 	var width = window.innerWidth;
 	
 	if(width>702 )
@@ -57,3 +42,34 @@ window.addEventListener("resize",function(){
 
 	}
 });
+ 	 			
+	burger.onclick=function(){
+		
+
+	if(conteinerForLSB.style.display =='none')
+	{
+		main.style.gridTemplateColumns='1fr 3fr';
+		conteinerForLSB.style.display='block';
+		conteinerForLSB.style.gridColumnStart='1';
+		MainContent.style.gridColumnStart='2';
+		Advirstment.style.display ='none';
+		
+
+	}
+
+	else if(conteinerForLSB.style.display=='block'){
+
+
+		main.style.gridTemplateColumns='3fr 1fr';
+		conteinerForLSB.style.display ='none';	
+		MainContent.style.gridColumnStart='1';
+		Advirstment.style.display ='block';
+		Advirstment.style.gridColumnStart='2';
+	}
+
+
+	
+};
+
+
+
