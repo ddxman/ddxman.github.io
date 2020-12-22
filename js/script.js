4939742ff395834210c24dcfd9d8de7bf7fbf4bd
ddxman <76228291+ddxman@users.noreply.github.com>
2020-12-22 08:23:55 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ if(clientidth<702)
 	if(width>702 )
 	{
 		main.style.gridTemplateColumns='2fr 3fr 2fr';
-		conteinerForLSB.style.display='grid';
+		conteinerForLSB.style.display='block';
 		conteinerForLSB.style.gridColumnStart='1';
 		MainContent.style.gridColumnStart='2';
 		Advirstment.style.display ='grid';
@@ -47,7 +47,7 @@ if(clientidth<702)
 	if(conteinerForLSB.style.display =='none')
 	{
 		main.style.gridTemplateColumns='2fr 4fr';
-		conteinerForLSB.style.display='grid';
+		conteinerForLSB.style.display='block';
 		conteinerForLSB.style.gridColumnStart='1';
 		MainContent.style.gridColumnStart='2';
 		Advirstment.style.display ='none';
