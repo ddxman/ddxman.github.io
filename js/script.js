8fc5ac1c947835d08fe3518b239cc222a0feb6d5
ddxman <76228291+ddxman@users.noreply.github.com>
2020-12-22 08:22:02 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,15 +2,13 @@ var conteinerForLSB = document.getElementById('conteinerForLSB');
 var burger = document.getElementById('burger');
 var advirstment = document.querySelector('Advirstment');
 var mainContent = document.getElementById('MainContent');
-
-
 var clientidth = document.documentElement.clientWidth;	
 
 if(clientidth<702)
 {
 	main.style.gridTemplateColumns ='4fr 1fr';
 	conteinerForLSB.style.display='none';
-	Advirstment.style.display ='block';
+	Advirstment.style.display ='grid';
 	MainContent.style.gridColumnStart='1';
 	Advirstment.style.gridColumnStart='2';
 
@@ -22,7 +20,7 @@ if(clientidth<702)
 	if(width>702 )
 	{
 		main.style.gridTemplateColumns='2fr 3fr 2fr';
-		conteinerForLSB.style.display='block';
+		conteinerForLSB.style.display='grid';
 		conteinerForLSB.style.gridColumnStart='1';
 		MainContent.style.gridColumnStart='2';
 		Advirstment.style.display ='grid';
@@ -49,7 +47,7 @@ if(clientidth<702)
 	if(conteinerForLSB.style.display =='none')
 	{
 		main.style.gridTemplateColumns='2fr 4fr';
-		conteinerForLSB.style.display='block';
+		conteinerForLSB.style.display='grid';
 		conteinerForLSB.style.gridColumnStart='1';
 		MainContent.style.gridColumnStart='2';
 		Advirstment.style.display ='none';
@@ -57,7 +55,7 @@ if(clientidth<702)
 
 	}
 
-	else if(conteinerForLSB.style.display=='block'){
+	else if(conteinerForLSB.style.display=='grid'){
 
 
 		main.style.gridTemplateColumns='4fr 1fr';
